3911bb9d71ad64af39d53eaafc02acaab3ff68ce
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-09-16 14:43:54 +0300

    register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,18 +2,25 @@ import { Footer } from "../components/footer/Footer"
 import { Header } from "../components/header/Header"
 
 export function Register() {
-
     return (
         <>
             <Header />
-            <main>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <h1>Register</h1>
-                            <p>Register</p>
+            <main className="form-signin w-100 m-auto">
+                <div className="row">
+                    <form className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+                        <div className="form-floating">
+                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                            <label htmlFor="floatingInput">Email address</label>
                         </div>
-                    </div>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+
+                        <button className="btn btn-primary w-100 py-2 mt-3" type="submit">Sign in</button>
+                    </form>
                 </div>
             </main>
             <Footer />
