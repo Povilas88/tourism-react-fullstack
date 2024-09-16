a07c20aedcc4d34bbf44fbb18f4f5d8c2f9d09db
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-09-16 13:15:24 +0300

    db reconnect

--- server/db.js
+++ server/db.js
@@ -1,8 +1,32 @@
 import mysql from 'mysql2/promise';
 
-export const connection = await mysql.createConnection({
+const dbOptions = {
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'tourism',
-});
+};
+
+export let connection = null;
+
+try {
+    connection = await mysql.createConnection(dbOptions);
+    console.log('Working');
+
+} catch (error) {
+    console.log('Cant get Database, turn on XAMP');
+
+}
+
+setInterval(async () => {
+    if (connection?.connection?._fatalError !== null) {
+        try {
+            connection = await mysql.createConnection(dbOptions);
+        } catch {
+            console.log('Cant get Database, turn on XAMP');
+        }
+    } else {
+        console.log('Connection: ok');
+
+    }
+}, 10000)
--- server/router/locations/locations.js
+++ server/router/locations/locations.js
@@ -3,48 +3,20 @@ import { connection } from '../../db.js';
 
 export const locationsAPIrouter = express.Router();
 
-locationsAPIrouter.get('/', async (req, res) => {
-    const data = [
-        {
-            img: '/',
-            name: 'Moon',
-            address: {
-                country: 'Space',
-                city: 'A',
-                street: 'A',
-                number: 'A',
-                zip: 'A',
-            },
-        },
-        {
-            img: '/',
-            name: 'Flower',
-            address: {
-                country: 'Tundra',
-                city: 'B',
-                street: 'B',
-                number: 'B',
-                zip: 'B',
-            },
-        },
-        {
-            img: '/',
-            name: 'Jellyfish',
-            address: {
-                country: 'Baltic sea',
-                city: 'C',
-                street: 'C',
-                number: 'C',
-                zip: 'C',
-            },
-        },
-    ];
+locationsAPIrouter.get('/', getLocations)
 
+async function getLocations(req, res) {
     const sql = 'SELECT * FROM locations;';
-    const dataFromServer = await connection.execute(sql);
+    let dataFromServer = null;
+
+    try {
+        dataFromServer = await connection.execute(sql);
+    } catch (error) {
+        dataFromServer = [[]];
+    }
 
     return res.status(200).json({
         status: 'success',
         data: dataFromServer[0],
     });
-});
+};
