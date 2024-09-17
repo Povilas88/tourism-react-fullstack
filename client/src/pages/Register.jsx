11c471231099d261dd39d550e39e2cd19c810a60
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-09-17 13:37:19 +0300

    register form validation

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,22 +1,64 @@
-import { Footer } from "../components/footer/Footer"
-import { Header } from "../components/header/Header"
+import { useState } from "react";
+import { Footer } from "../components/footer/Footer";
+import { Header } from "../components/header/Header";
 
 export function Register() {
+    const minUsernameLength = 3;
+    const maxUsernameLength = 20;
+    const minPasswordLength = 8;
+    const maxPasswordLength = 100;
+
+    const [username, setUsername] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [isFormValidated, setIsFormValidated] = useState(false);
+
+    function submitForm(e) {
+        e.preventDefault();
+        setIsFormValidated(true);
+
+        let usernameError = '';
+
+        if (username.length < minUsernameLength) {
+            usernameError = `Username is too short, must be atleast ${minUsernameLength} symbols.`;
+        } else if (username.length > maxUsernameLength) {
+            usernameError = `Username is too long, must be a maximum of ${maxUsernameLength} symbols.`;
+        }
+        setUsernameError(usernameError);
+
+        let passwordError = '';
+        if (password.length < minPasswordLength) {
+            passwordError = `Password is too short, must be atleast ${minPasswordLength} symbols.`;
+        } else if (password.length > maxPasswordLength) {
+            passwordError = `Password is too long, must be a maximum of ${maxPasswordLength} symbols.`;
+        }
+        setPasswordError(passwordError);
+    }
+
     return (
         <>
             <Header />
             <main className="form-signin w-100 m-auto">
                 <div className="row">
-                    <form className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    <form onSubmit={submitForm} className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <h1 className="h3 mb-3 fw-normal">Please register</h1>
 
                         <div className="form-floating">
-                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label htmlFor="floatingInput">Email address</label>
+                            <input value={username} onChange={e => setUsername(e.target.value.trim())}
+                                type="text" id="username" placeholder="Username"
+                                className={'form-control ' + (isFormValidated ? usernameError ? 'is-invalid' : 'is-valid' : '')}
+                            />
+                            <label htmlFor="username">Username</label>
+                            {/* {usernameError ? <p>{usernameError}</p> : null} */}
+                            {usernameError && <p className="invalid-feedback">{usernameError}</p>}
                         </div>
                         <div className="form-floating">
-                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                            <label htmlFor="floatingPassword">Password</label>
+                            <input value={password} onChange={e => setPassword(e.target.value.trim())}
+                                type="password" id="password" placeholder="Password"
+                                className={'form-control ' + (isFormValidated ? passwordError ? 'is-invalid' : 'is-valid' : '')} />
+                            <label htmlFor="password">Password</label>
+                            {passwordError && <p className="invalid-feedback">{passwordError}</p>}
                         </div>
 
                         <button className="btn btn-primary w-100 py-2 mt-3" type="submit">Sign in</button>
